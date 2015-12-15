c94c6c9ff23f85236ae2a49951dc8210d20ea45c
Ruchandani, Sagar <Sagar.Ruchandani@emc.com>
2015-12-14 18:13:25 -0800

    output file timestamp

--- 295B-WebModule_v_2/DataAnalyzer/src/main/java/edu/sjsu/dataanalyzer/utils/pyAlgorithms.py
+++ 295B-WebModule_v_2/DataAnalyzer/src/main/java/edu/sjsu/dataanalyzer/utils/pyAlgorithms.py
@@ -7,9 +7,10 @@ from sklearn import ensemble
 from sklearn.cross_validation import train_test_split
 import sys
 import os
+import datetime
 #from pymongo import MongoClient
 #print os.path.join(os.path.expanduser('~'),"test_output.csv")
-print ">>>>>",os.path.join(os.path.expanduser('~'),"test_output.csv"),"<<<<<"
+print ">>>>>",os.path.join(os.path.expanduser('~'),sys.argv[2]+datetime.datetime.now().strftime('%Y%m%d%H%M%S')+".csv"),"<<<<<"
 #client = MongoClient()
 #client = MongoClient('localhost', 27017)
 #db = client.cmpedb
@@ -69,4 +70,4 @@ elif(sys.argv[1]=='GRADIENT_BOOSTING'):
     elif(sys.argv[7]=="rounded"):
         print(y_3.astype(np.int64))
         test_data[outputCol]=y_3.astype(np.int64)
-test_data.to_csv(os.path.join(os.path.expanduser('~'),"test_output.csv"))
+test_data.to_csv(os.path.join(os.path.expanduser('~'),sys.argv[2]+datetime.datetime.now().strftime('%Y%m%d%H%M%S')+".csv"))
